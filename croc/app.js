02cbbcc7e668098c089847e241a151d10877b9e4
Pavel Mazhuga <Pavel@iMac-Pavel.local>
2020-09-30 11:49:30 +0700

    .

--- croc/app.js
+++ croc/app.js
@@ -273,7 +273,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
         scrollContainer.addEventListener('scroll', () => {
             const scrollY = scrollContainer.scrollTop;
-            y = Math.min(scrollY * 0.95, windowHeight - 80);
+            y = Math.min(scrollY * 0.95, windowHeight - 40);
         });
     }
 
